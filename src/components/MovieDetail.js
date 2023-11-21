9ac9bd753b5635ab850f21bd76d6cba66859ba86
Astra <abhaypratapsinghwd@gmail.com>
2023-11-21 19:51:13 +0530

    Layout of Movie Detail

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -3,19 +3,33 @@ import { useParams } from 'react-router-dom'
 
 const MovieDetail = () => {
   const { id } = useParams();
-  const [movie,setMovie] = useState();
+  const [movie, setMovie] = useState();
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`)
-    .then(res=>res.json())
-    .then(movie=>{setMovie(movie)})
-  })
+      .then(res => res.json())
+      .then(movie => {
+        console.log(movie);
+        setMovie(movie)
+      })
+  }, [id])
   return (
-    <div>{movie?
-      <div>
-        <p>{movie.title}</p>
-        <p>{movie.overview}</p>
+    <div>{movie ?
+      <div className='movie-container'>
+        <div className='movie-image-container'>
+          <img src={"https://image.tmdb.org/t/p/w500" + movie?.poster_path} alt='movie' />
+          <p>{movie.status}</p>
+        </div>
+        <div className='movie-info'>
+          <h4>{movie.title}</h4>
+          <ul>
+            {movie.genres.map(genre => {
+              return <li key={genre.id}>{genre.name}</li>
+            })}
+          </ul>
+          <p>{movie.overview}</p>
+        </div>
       </div>
-      :<h3>Loading...</h3>}</div>
+      : <h3>Loading...</h3>}</div>
   )
 }
 
