54dfc16544190a9a45d95b7f90206f0d42c1dc66
Astra <abhaypratapsinghwd@gmail.com>
2023-11-21 19:35:04 +0530

    Added basic Movie Detail Page

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const MovieDetail = () => {
-  const {id} = useParams();
+  const { id } = useParams();
+  const [movie,setMovie] = useState();
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`)
+    .then(res=>res.json())
+    .then(movie=>{setMovie(movie)})
+  })
   return (
-    <div>{id}</div>
+    <div>{movie?
+      <div>
+        <p>{movie.title}</p>
+        <p>{movie.overview}</p>
+      </div>
+      :<h3>Loading...</h3>}</div>
   )
 }
 
 export default MovieDetail
+
